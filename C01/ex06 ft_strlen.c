3c572ba84eb7d7db218f397531641dbbd3a199b6
Mikhail Poma <mpoma@made-f0Ar12s9.clusters.42paris.fr>
2024-03-19 12:38:20 +0100

    ex06

---
+++ C01/ex06 ft_strlen.c
@@ -0,0 +1,6 @@
+int main(void)
+{
+	char str[] = "9876543210";
+	printf("strlen is %d", ft_strlen(str));
+	return 0;
+}
--- C01/tests.py
+++ C01/tests.py
@@ -33,9 +33,9 @@ def compile_with_testcode(root, dirname, filename, sourcedir):
 
     return output_
 
-exercises = ["ex0" + str(i) for i in range(6)]
+exercises = ["ex0" + str(i) for i in range(7)]
 filenames = ["ft_ft.c", "ft_ultimate_ft.c", "ft_swap.c", "ft_div_mod.c",
-             "ft_ultimate_div_mod.c", "ft_putstr.c"]
+             "ft_ultimate_div_mod.c", "ft_putstr.c", "ft_strlen.c"]
 
 for exer_, file_ in zip(exercises, filenames):
     print (compile_with_testcode("../..", exer_, file_, "source_files"))
