b6ce8c36e89eb1bd80ea65db296a082763327459
Mikhail Poma <mpoma@made-f0Ar12s9.clusters.42paris.fr>
2024-03-19 13:11:27 +0100

    added test case for ex07

--- C01/ex07 ft_rev_int_tab.c
+++ C01/ex07 ft_rev_int_tab.c
@@ -1,11 +1,19 @@
 int main(void)
 {
-	int tab[5] = {0,1,2,3,4};
+	int tab_5[5] = {0,1,2,3,4};
 
-	ft_rev_int_tab(tab, 5);
+	ft_rev_int_tab(tab_5, 5);
 	for (int i = 0; i < 5; i++)
 	{
-		printf ("%d, ", tab[i]);
+		printf ("%d, ", tab_5[i]);
 	}
-	printf("\n");
+	printf(" ");
+
+	int tab_4[4] = {0,1,2,3};
+	ft_rev_int_tab(tab_4, 4);
+	for (int i = 0; i < 4; i++)
+	{
+		printf ("%d, ", tab_4[i]);
+	}
+	return 0;
 }
