564ca943502215266e57de999ef06399a43d211d
Mikhail Poma <mpoma@made-f0Cr4s3.clusters.42paris.fr>
2024-03-19 16:41:28 +0100

    implemented C01 test function to C00

--- C00/tests.py
+++ C00/tests.py
@@ -1,20 +1,41 @@
 import os
 import subprocess
 
-folder = "../.."
+def compile_with_testcode(root, dirname, filename, sourcedir):
+    try:
+        with open(f"{dirname} {filename[:-1]+'h'}", "r") as f:
+            head_ = f.read()
+    except:
+        head_ = ""
+    with open(f"{dirname} {filename}", "r") as f:
+        tail_ = f.read()
+    
+    source_file_in = os.path.join(root, dirname, filename)
+    with open(source_file_in, 'r+') as f_in:
+        content = f_in.read()
+        #f_in.seek(0,0)
+        #f_in.write(head_.rstrip('\r\n') + '\n' + content + '\n' + tail_)
+        if not os.path.exists(sourcedir):
+            os.makedirs(sourcedir)
+        source_file_out = os.path.join(sourcedir, filename)
+        with open(source_file_out, "w") as f_out:
+            f_out.write(head_.rstrip('\r\n') + '\n' + content + '\n' + tail_)
 
-norm_check = subprocess.check_output(f'norminette "{folder}"', shell=True).decode('utf-8')
 
-if not set([line_[-3:] for line_ in norm_check.split('\n')]) - {"", "OK!"}:
-    print ("Norminette check OK!", end="\n\n")
-else:
-    print (norm_check)
+    gcc_command = ["gcc", "-Wall", "-Wextra", "-Werror", "-o", "my_program", source_file_out]
+    try:
+        subprocess.run(gcc_command, check=True)
+        print(f"{filename} compilation successful")
+    except subprocess.CalledProcessError as e:
+        print(f"{filename} compilation failed:", e)
+        
+    output_ = subprocess.check_output("./my_program", shell=True)
 
-possible_dirs = set(["ex0" + str(i) for i in range(9)])
-dirs_we_have = []
+    return output_
 
-for filename_ in sorted(os.listdir(folder)):
-    if filename_ in possible_dirs:
-        dirs_we_have.append(filename_)
+exercises = ["ex0" + str(i) for i in range(6)]
+filenames = ["ft_putchar.c", "ft_print_alphabet.c", "ft_print_reverse_alphabet.c",
+             "ft_print_numbers.c", "ft_is_negative", "ft_print_comb"]
 
-print (dirs_we_have)
+for exer_, file_ in zip(exercises, filenames):
+    print (compile_with_testcode("../..", exer_, file_, "source_files"))
