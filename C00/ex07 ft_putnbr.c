5ac189e753e58cf3c36f5ba5905a2f0c2bb092ed
Mikhail Poma <mpoma@made-f0Cr9s5.clusters.42paris.fr>
2024-03-20 11:42:21 +0100

    C00 tests complete

---
+++ C00/ex07 ft_putnbr.c
@@ -0,0 +1,8 @@
+int main(void)
+{
+	ft_putnbr(-32768);
+	ft_putnbr(0);
+	ft_putnbr(111);
+	ft_putnbr(32767);
+	return (0);
+}
---
+++ C00/ex08 ft_print_combn.c
@@ -0,0 +1,7 @@
+int main(void)
+{
+	ft_print_combn(1);
+	ft_print_combn(3);
+	ft_print_combn(9);
+	return (0);
+}
