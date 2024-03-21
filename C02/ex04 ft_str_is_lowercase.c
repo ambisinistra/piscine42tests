176332285cfa2b61e4338ac267c6854a871f8d35
Mikhail Poma <mpoma@made-f0Br4s10.clusters.42paris.fr>
2024-03-21 11:29:41 +0100

    minor typose correction

--- C02/ex04 ft_str_is_lowercase.c
+++ C02/ex04 ft_str_is_lowercase.c
@@ -1,10 +1,10 @@
 int	main(void)
 {
-  int size_ = 4;
-  char test_cases[4][4] = {"aoe", "AoE", "@", "", "1"};
+  int size_ = 5;
+  char test_cases[5][4] = {"aoe", "AoE", "@", "", "1"};
   for (int i=0; i < size_; i++)
   {
-    printf("%s %d\n", ft_str_is_lowercase(test_cases[i]));
+	  printf("%s %d ||", test_cases[i], ft_str_is_lowercase(test_cases[i]));
   }	 
   return (0);
 }
