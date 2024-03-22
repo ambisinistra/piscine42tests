8b5995cf8079d3b4c451d4145937c070dec77db4
Mikhail Poma <mpoma@made-f0Br1s2.clusters.42paris.fr>
2024-03-22 11:29:07 +0100

    test C02 corrections

--- C02/ex07 ft_strupcase.c
+++ C02/ex07 ft_strupcase.c
@@ -3,9 +3,9 @@ int main(void)
   int size_ = 1;
   char test_cases[1][10] = {"iDhTns123@"};
 
-  for (int i=0; i < size_, i++)
+  for (int i=0; i < size_; i++)
   {
-    printf ("%s \n", test_cases[i]);
+	  printf ("%s \n", ft_strupcase(test_cases[i]));
   }
   return (0);
 }
--- C02/ex09 ft_strcapitalize.c
+++ C02/ex09 ft_strcapitalize.c
@@ -1,11 +1,11 @@
 int main(void)
 {
   int size_ = 1;
-  int test_cases[1][50] = {"salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un"};
+  char	test_cases[1][100] = {"salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un"};
 
   for (int i=0; i < size_; i++)
   {
-    printf ("%s \n", test_cases[i]);
+	  printf ("%s \n", ft_strcapitalize(test_cases[i]));
   }
   return (0);
 }
