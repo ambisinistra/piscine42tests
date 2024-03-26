8633cd5788d11c93091531027b638c5951bcecba
Mikhail Poma <mpoma@made-f0Ar8s9.clusters.42paris.fr>
2024-03-26 19:22:30 +0100

    C02EX12 modified test function

--- C02/ex12 ft_print_memory.c
+++ C02/ex12 ft_print_memory.c
@@ -1,5 +1,9 @@
 int main(void)
-{
-  ft_print_memory();
-  return (0);
+{	
+	char str[] = "Bonjour les aminches...c. est fou.to";
+	for (int i=0; i < 40; i++)
+	{
+		printf ("-------------- iteration %d -----------------", i);
+		ft_print_memory(str, i);
+	}
 }
--- C02/tests.py
+++ C02/tests.py
@@ -38,8 +38,9 @@ def compile_with_testcode(root, dirname, filename, sourcedir):
 exercises = ["ex" + str(i//10) + str(i%10) for i in range(13)]
 filenames = ["ft_strcpy.c", "ft_strncpy.c", "ft_str_is_alpha.c", "ft_str_is_numeric.c",
              "ft_str_is_lowercase.c", "ft_str_is_uppercase.c", "ft_str_is_printable.c",
-             "ft_strupcase.c", "ft_strlowcase.c", "ft_strcapitalize.c", "ft_strlcpy.c"
+             "ft_strupcase.c", "ft_strlowcase.c", "ft_strcapitalize.c", "ft_strlcpy.c",
              "ft_putstr_non_printable.c", "ft_print_memory.c"]
 
 for exer_, file_ in zip(exercises, filenames):
+    print (exer_, file_)
     print (compile_with_testcode("../..", exer_, file_, "source_files"))
