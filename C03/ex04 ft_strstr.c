85dc23d0f05be7ac691f262c0b5f4b9d4f8dcc90
Mikhail Poma <mpoma@made-f0Ar3s7.clusters.42paris.fr>
2024-03-26 10:26:53 +0100

    added new C03EX04 test

--- C03/ex04 ft_strstr.c
+++ C03/ex04 ft_strstr.c
@@ -1,6 +1,19 @@
 int main(void)
 {
-  char dest[] = ".... !! where to find my substring???@@@!";
-  char src[] = "my substring";
-  printf ("%s\n%s\n", dest, ft_strstr(dest, src));
+	int size_ = 4;
+	char dest_1[4][50] = {".... !! where to find my substring???@@@!",
+		" where is my substring?", "", ""};
+	char dest_2[4][50] = {".... !! where to find my substring???@@@!",
+		" where is my substring?", "", ""};
+	char src_1[4][20] = {"my substring", "", "  12345", ""};
+	char src_2[4][20] = {"my substring", "", "  12345", ""};
+	for (int i=0; i < size_; i++)
+	{
+		printf ("original strstr\n");
+		printf ("%s\n%s\n%s\n-------\n", dest_1[i],
+				src_1[i], strstr(dest_1[i], src_1[i]));
+		printf ("ft strstr\n");
+		printf ("%s\n%s\n%s\n-------\n", dest_2[i],
+				src_2[i], ft_strstr(dest_2[i], src_2[i]));
+	}
 }
